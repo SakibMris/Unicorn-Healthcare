8e11a0235ea1442620b38783e122b4e8fe46e90d
SakibMris <sakibmris@gmail.com>
2021-10-20 23:22:01 +0600

    DONE

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -14,14 +14,24 @@ const Login = () => {
     }
     return (
         <div className="m-5">
-            <h1 className="m-4">Please Login</h1>
-            <input onBlur={emailChange} className="w-25" type="email" name="email" placeholder="Email" id="1" /><br /><br />
-            <input onBlur={passwordChange} className="w-25" type="password" name="password" placeholder="Password" id="2" />
-            <br /><br /><br />
-            <button onClick={logInUsingEmail} type="button" class="btn btn-success me-4">Login</button>
-            <button type="button" class="btn btn-primary">Reset</button>
-            <br /><br />
-            <button className="btn btn-danger px-5" onClick={handleGoogleLogin}> Google</button>
+            <form onSubmit={logInUsingEmail}>
+                <h1 className="m-4">Please Login</h1>
+                <div className="row mb-3 w-100">
+                    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input onBlur={emailChange} type="email" className="form-control" id="inputEmail3" />
+                    </div>
+                </div>
+                <div className="row mb-3 w-100">
+                    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input onBlur={passwordChange} type="password" className="form-control" id="inputPassword3" />
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button><br /><br />
+                <div>-----------------------------------</div>
+                <button className="btn btn-danger px-5" onClick={handleGoogleLogin}> Google</button>
+            </form>
         </div>
     );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,24 +1,26 @@
 import useFirebase from '../../Firebase/useFirebase';
 
 const Register = () => {
-    const { handleRegistration, emailChange, passwordChange } = useFirebase()
+    const { handleRegistration, emailChange, passwordChange, handleGoogleLogin } = useFirebase()
     return (
         <div className="m-5">
             <form onSubmit={handleRegistration}>
                 <h1 className="m-5">Please Register</h1>
-                <div className="row mb-3">
+                <div className="row mb-3 w-100">
                     <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
                         <input onBlur={emailChange} type="email" className="form-control" id="inputEmail3" />
                     </div>
                 </div>
-                <div className="row mb-3">
+                <div className="row mb-3 w-100">
                     <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
                         <input onBlur={passwordChange} type="password" className="form-control" id="inputPassword3" />
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Register</button>
+                <button type="submit" className="btn btn-primary">Register</button><br /><br />
+                <div>-----------------------------------</div>
+                <button className="btn btn-danger px-5" onClick={handleGoogleLogin}> Google</button>
             </form>
         </div>
     );
